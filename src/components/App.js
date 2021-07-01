9321a0f6d03b95fd9aa0e1541fa85c0e278042a1
Smith, Leon <leon.smith@tyson.com>
2021-06-30 21:39:06 -0500

    Add call to Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,42 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
+    state = { images: [], errorMessage: '' }
     
-    onSearchSubmit(term) {
-        console.log(term);
+    // turn from method to arrow function
+    onSearchSubmit = (term) => {
+
+        //Set access key to api in header
+        const headers = {'Authorization': 'Client-ID qkz4NXgJZhwhHjUAIpqMKMx4SqstZyEoE7ZnkQ9OBJs'}
+        const params = {query: term}
+
+        //Refactor to remove config code 
+        fetch('https://api.unsplash.com/search/photos?' + new URLSearchParams({query: term}), { headers: headers })
+            .then(async response => {
+                const data = await response.json();
+                
+                //Check for error message
+                if (!response.ok) {
+                    // get error message from body or default to response statusText
+                    const error = (data && data.message) || response.statusText;
+                    return Promise.reject(error);
+                }
+
+                //Set the state object
+                this.setState({ images: data.results });
+            })
+            .catch(error => {
+                this.setState({ errorMessage: error });
+                console.error('There was an error!', error);  
+            });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '1%' }}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         );
     }
